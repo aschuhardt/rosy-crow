ab05b6215830809a6785063000ff3033a83b9c5d
Addison Schuhardt <addison@schuhardt.net>
2023-05-31 22:15:01 -0700

    remove unused method

--- Services/Identity/IIdentityService.cs
+++ Services/Identity/IIdentityService.cs
@@ -6,7 +6,6 @@ public interface IIdentityService
 {
     public X509Certificate2 ActiveCertificate { get; }
     bool ShouldReloadActiveCertificate { get; }
-    Task<byte[]> DerivePassword(Models.Identity identity);
     void ClearActiveCertificate();
     Task Activate(Models.Identity identity);
     Task<X509Certificate2> LoadActiveCertificate();

c4da8ce8f3829925b8b0e650ef2d48435d4c9186
Addison Schuhardt <addison@schuhardt.net>
2023-10-18 12:31:21 -0700

    correctly handle deep-linking intents on app startup

--- Platforms/Android/MainActivity.cs
+++ Platforms/Android/MainActivity.cs
@@ -1,12 +1,14 @@
-﻿using Android.App;
+﻿using System.Diagnostics;
+using Android.App;
 using Android.Content;
 using Android.Content.PM;
+using Android.OS;
 using RosyCrow.Models;
 
 namespace RosyCrow.Platforms.Android;
 
 [Activity(Theme = "@style/Maui.SplashTheme",
-    MainLauncher = true,
+    MainLauncher = true, Exported = true,
     LaunchMode = LaunchMode.SingleInstance,
     ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode |
                            ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
@@ -23,9 +25,24 @@ public class MainActivity : MauiAppCompatActivity
 {
     protected override void OnNewIntent(Intent intent)
     {
+        if (intent == null)
+            return;
+
         if (intent.Action == Intent.ActionView && intent.Data != null)
             (Microsoft.Maui.Controls.Application.Current as App)?.HandleNavigationIntent(intent.Data.ToString());
 
         base.OnNewIntent(intent);
+    }
+
+    protected override void OnCreate(Bundle savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+
+        var intent = Intent;
+        if (intent == null)
+            return;
+
+        if (intent.Action == Intent.ActionView && intent.Data != null)
+            (Microsoft.Maui.Controls.Application.Current as App)?.HandleNavigationIntent(intent.Data.ToString());
     }
 }
